0085ff5774faa5a076e6e79a02adf9c5cc61563b
Dima <byvalyi-dima@mail.ru>
2021-08-31 13:47:39 +0300

    first generation

--- 1.py
+++ 1.py
@@ -1,1 +1,21 @@
-rgdffgrgdgdf
+list = [15, 48, "hello", 6, 19, "world"]
+sum = 0
+c = 0
+d = 0
+for i in list:
+    if type(i) is  int and i%2 == 0:
+        for j in str(i):
+            j=int(j)
+            sum+=j
+    if type(i) is int and not i % 2 == 0:
+        list[list.index(i)]=1
+    elif type(i) is str:
+        for x in i:
+            if x in 'aeoy':
+                c=c+1
+            else:
+                d=d+1
+print(sum)
+print(list)
+print(c)
+print(d)
